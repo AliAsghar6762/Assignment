45681375bb79afd01a8a1322c0a0c0c07caa78bd
AliAsghar6762 <113584234+AliAsghar6762@users.noreply.github.com>
2022-09-16 16:06:50 +0500

    Update Question5.cpp

--- Assignment/Question5.cpp
+++ Assignment/Question5.cpp
@@ -2,7 +2,7 @@
 //using namespace std;
 //int main()
 //{
-//	int rice=2205, bags, rb;
+//	int rice=2205, bags, rb;\\rb is the amount of rice a bag can store
 //	cout << "Enter the amount of rice that can be stored in one bag\n";
 //	cin >> rb;
 //	bags = rice / rb;
