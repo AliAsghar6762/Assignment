0d9a538388266513dc7d19c87ee53b62b8534028
AliAsghar6762 <113584234+AliAsghar6762@users.noreply.github.com>
2022-09-16 16:20:19 +0500

    Update Question21.cpp

--- Assignment/Question21.cpp
+++ Assignment/Question21.cpp
@@ -3,17 +3,17 @@
 //using namespace std;
 //int main()
 //{
-//	float x, y, t, h, s1, s2;
+//	float s1, s2, t, distance, d1, d2;
 //	cout << "Enter the average speed of car A\n";
-//	cin >> x;
+//	cin >> s1;
 //	cout << "Enter the average speed of car B\n";
-//	cin >> y;
+//	cin >> s2;
 //	cout << "Enter elapsed time\n";
 //	cin >> t;
-//	s1 = x * t;
-//	s2 = y * t;
-//	h = sqrt((s1 * s1) + (s2 * s2));
-//	cout << "Distance between the cars\n" << h << "\n";
+//	d1 = s1 * t;
+//	d2 = s2 * t;
+//	distance = sqrt((d1 * d1) + (d2 * d2));
+//	cout << "Distance between the cars\n" << distance << "\n";
 //
 //
 //	system("pause");
